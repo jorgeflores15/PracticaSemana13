f36ce51533a3d92338d1759e5c7a401ca15982e5
Linder Hassinger <linderhassinger00@gmail.com>
2017-11-13 12:46:40 -0500

    Initial commit

---
+++ app/src/main/java/com/linder/logginsemana14/Service/ApiService.java
@@ -0,0 +1,43 @@
+package com.linder.logginsemana14.Service;
+
+import com.linder.logginsemana14.model.Producto;
+import com.linder.logginsemana14.model.User;
+
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+
+/**
+ * Created by linderhassinger on 11/13/17.
+ */
+
+public interface ApiService {
+
+    String API_BASE_URL = "https://productos-api-linder3hs.c9users.io/";
+
+    @GET("api/v1/loginUsers")
+    Call<List<User>> getUsers();
+
+
+    @GET("api/v1/productos")
+    Call<List<Producto>> getProductos();
+
+    @FormUrlEncoded
+    @POST("/api/v1/login")
+    Call<ResponseMessage> loginUser
+                (@Field("username")String nombre,
+                 @Field("password") String password
+    );
+
+
+}
